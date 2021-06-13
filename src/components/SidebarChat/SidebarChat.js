86b9aa75c8f8e482a9fb6f39fcda8ef1efc7eabb
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-06-13 18:50:00 +0600

    kichuta korsi

--- src/components/SidebarChat/SidebarChat.js
+++ src/components/SidebarChat/SidebarChat.js
@@ -1,10 +1,18 @@
 import { Avatar } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import './SidebarChat.css';
 
 const SidebarChat = () => {
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
   return (
     <div className="sidebarChat">
-      <Avatar src="https://avatars.dicebear.com/api/human/a23.svg" />
+      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}}.svg`} />
       <div className="sidebarChat_info">
         <h2>Room name</h2>
         <p>Last message</p>
