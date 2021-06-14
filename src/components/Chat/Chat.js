ab4612d81d958aeefebe3eaa39e753352ec5e9d4
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-06-14 18:52:02 +0600

    keep trying

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import './Chat.css';
+import { Avatar, IconButton } from "@material-ui/core";
+import React from "react";
+import "./Chat.css";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 
 const Chat = () => {
-    return (
-        <div className="chat">
-            <h1>This is chat</h1>
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar />
+
+        <div className="chat_headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
         </div>
-    );
+
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
